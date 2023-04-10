0c9ea1ec1d74d5273f8bd80dc63abf9e54a3d1c3
vasiltop <vasiltopalovic@outlook.com>
2023-04-10 13:01:19 -0400

    images

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-3 px-6 gap-6"> 
 
     <div class="card bg-base-300 shadow-xl">
-        <figure><img src="/src/routes/projects/pathfinding.PNG" alt="pic" /></figure>
+        <figure><img src="/images/pathfinding.PNG" alt="pic" /></figure>
         <div class="card-body">
             <h2 class="card-title underline"> <a href="https://github.com/vasiltop/pathfinding_visualizer" target="_blank" rel="noopener noreferrer"> A-Star Pathfinding</a></h2>
             <p>Solves any maze with the most efficient path using Python and the A-Star algorithm.</p>
@@ -9,7 +9,7 @@
     </div>
 
     <div class="card bg-base-300 shadow-xl">
-        <figure><img src="/src/routes/projects/website.jfif" alt="pic" /></figure>
+        <figure><img src="/images/website.jfif" alt="pic" /></figure>
         <div class="card-body">
             <h2 class="card-title underline"> <a href="https://github.com/vasiltop/personal_website" target="_blank" rel="noopener noreferrer"> Personal Website </a></h2>
             <p>A website built using Svelte.</p>
@@ -17,7 +17,7 @@
     </div>
 
     <div class="card bg-base-300 shadow-xl">
-        <figure><img src="/src/routes/projects/game.PNG" alt="pic"></figure>
+        <figure><img src="/images/game.PNG" alt="pic"></figure>
         <div class="card-body">
             <h2 class="card-title underline"> <a href="https://github.com/vasiltop/winter_vendetta_game" target="_blank" rel="noopener noreferrer"> Video Game </a></h2>
             <p>My video game "Winter Vendetta" was created using Gamemaker Studio and GML.</p>
