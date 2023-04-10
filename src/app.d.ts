ab29af26b3bc446a948d2c79cbaba7e2faac3938
vasiltop <vasiltopalovic@outlook.com>
2023-04-10 12:42:25 -0400

    cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,15 +7,7 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 
-		   interface Platform {
-		       env: {
-		           COUNTER: DurableObjectNamespace;
-		       };
-		       context: {
-		           waitUntil(promise: Promise<any>): void;
-		       };
-		       caches: CacheStorage & { default: Cache }
-			}
+		   
 	}
 }
 
