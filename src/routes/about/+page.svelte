93767f35b1ce12c7fb620b137524e8dc2120aca5
vasiltop <vasiltopalovic@outlook.com>
2023-04-10 13:45:53 -0400

    typo

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,7 @@
     <div class="hero-content text-center">
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">About Me</h1>
-        <p class="py-6">I am a first year student at the University of Ottawa that has always been interested in technology. I started programming in the 9th grade with game development and moved on Java and Python where I developed a deep passion for coding.
+        <p class="py-6">I am a first year student at the University of Ottawa that has always been interested in technology. I started programming in the 9th grade with game development and moved onto Java and Python where I developed a deep passion for coding.
             <br>
             <br>
             After continuing to learn for many years I participated in events such as the Waterloo Computing Contest, and the Engineering Idol, where I built a water filter for indigenous communities using Arduino.
