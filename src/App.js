5c6d9f1a2c9a510fe95bea84ded89bcb06a3ea58
vasiltop <vasiltopalovic@outlook.com>
2023-07-16 00:09:57 -0400

    Done Styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Navbar from './components/Navbar/Navbar';
 import Info from './components/Info/Info';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <body>
+
+      <Helmet> 
+        <title>Vasil Topalovic</title>
+      </Helmet>
+      
       <Navbar />
       <Info />
     </body>
