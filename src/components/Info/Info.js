01dd1c40bbf30b60eaa0cf76d3b6c33387bab9cb
vasiltop <vasiltopalovic@outlook.com>
2023-07-17 12:18:49 -0400

    Email contact

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -8,8 +8,9 @@ function Info() {
                 <h1>Welcome!</h1>
                 <div className="border-left-gradient"> 
 
-                    <p>I am a first year student at the University of Ottawa that has always been interested in technology. I started programming in the 9th grade with game development and moved onto Java and Python where I developed a deep passion for coding.</p>
+                    <p>I am a second year student at the University of Ottawa that has always been interested in technology. I started programming in the 9th grade with game development and moved onto Java and Python where I developed a deep passion for coding.</p>
                     <p> After continuing to learn for many years I participated in events such as the Waterloo Computing Contest, and the Engineering Idol, where I built a water filter for indigenous communities using Arduino.</p>
+                    <p>Feel free to contact me at <a> vasiltopalovic@outlook.com</a>.</p>
                 </div>
                 
 
